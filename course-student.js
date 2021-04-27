7a75c8da0a5251242764b52802908b7b6a526fe3
aya-98 <aya_ahmed0000424@outlook.com>
2021-04-27 22:44:54 +0200

    update

--- course-student.js
+++ course-student.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.set('view-engine' , 'ejs')
 
-app.use(express.urlencoded( { extended: false}))
+app.use(express.urlencoded( { extended: true}))
 
 app.use(express.json());
 
